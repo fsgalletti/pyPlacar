3e6a09a7a9eece093e186fd4ded3c34dc6fa5d1f
rogercorrea <roger.correa@TOP.TA.VALE.BR>
2016-09-15 08:42:11 -0300

    Atualização do código
    Inserção de comentários no código-fonte.

--- README.md
+++ README.md
@@ -2,6 +2,26 @@
 Busca os resultados da rodada de qualquer campeonato de futebol presente em um dos mais acessados sites de esportes do país.
 Pode ser usado principalmente para estudos com a linguagem Python, framework Flask, biblioteca LXML e outros.
 
+Instale em seu ambiente o Python 2.7, o Flask e as seguintes bibliotecas com seus respectivos comandos:
+
+sudo apt-get instapp python-lxml
+sudo apt-get instapp requests
+
 Para rodar, você pode executar o comando <b>python app.py</b> e ele irá criar um webserver na porta 80, com o app disponível.
 Você pode acessar o pyPlacar usando o endereço <b>http://seuip/futebol</b> .
 Caso necessite ou queira mudar a porta, acesse o arquivo app.py e altere o parâmetro port= na linha 56.
+
+Para buscar outra tabela de campeonato de futebol no mesmo site, você pode alterar o parâmetro da função <b>buscarResultados</b> na linha 15.
+Também pode escolher um time, caso queira retornar os jogos somente deste.
+
+Exemplo: Retornar jogos do Vasco na tabela da série B:
+<b>buscarResultados('brasileirao-serie-b', 'Vasco')</b>
+
+Para consultar o link dos torneios de futebol disponíveis, acesse a área de futebol do respectivo site e veja no link o nome do campeonato.
+Algumas opções:
+<b>brasileirao-serie-a<br>
+brasileirao-serie-b<br>
+brasileirao-serie-c<br>
+copa-do-brasil<br>
+copa-sul-americana</b>
+
--- app.py
+++ app.py
@@ -8,17 +8,23 @@ app = Flask(__name__)
 
 @app.route('/futebol')
 def futebol():
+	#A classe Markup renderiza o código HTML como parte construtiva da página, ao invés de tratar como conteúdo.
 	html = Markup(start());
    	return render_template('futebol.html', resultados=html);
 
 def start():
     return buscarResultados('brasileirao-serie-a', None);
 
+#buscarResultados
+#campeonato = Nome do campeonato de acordo com a URL disponível no site. Verifique na documentação os campeonatos disponíveis
+#clubeSelecionado = Insira o nome de um clube de futebol para filtrar para os jogos somente deste.
 def buscarResultados(campeonato, clubeSelecionado):
 
+	#Busca o conteúdo da página inteira
     page = requests.get('http://globoesporte.globo.com/futebol/' + campeonato + '/');
     page.encoding = 'utf-8'
     tree = html.fromstring(page.text);
+	#Usa o caminho Xpath para buscar os objetos com o conteúdo exclusivo da tabela da rodada do campeonato. Você pode usar o FirePath no Firefox para encontrar o caminho que precisa dentro do código HTML de qualquer página.
     jogos = tree.xpath(".//*[@id='container-para-tabela-simulador-ou-navegacao-js']/article/aside/ul/li");
     retorno = '<ul class="lista-de-jogos-conteudo">';
     for idx, item in enumerate(jogos):
@@ -27,15 +33,16 @@ def buscarResultados(campeonato, clubeSelecionado):
             visitante = jogos[idx].find("div/a/div[2]/span[3]/span[2]");
             if verificarClubeSelecionado(mandante, visitante, clubeSelecionado):
                 retorno += html.tostring(jogos[idx], pretty_print=True);
-
-            if (mandante is not None):
-                retorno += mandante.text + " x " + visitante.text;
         else:
             retorno += html.tostring(item, pretty_print=True);
 
     retorno += '</ul>';
     return retorno;
 
+#verificarClubeSelecionado
+#mandante = Clube mandante da partida
+#visitante = Clube visitante da partida
+#escolhido = Clube escolhido pelo programador para ser o único a retornar dados
 def verificarClubeSelecionado(mandante, visitante, escolhido):
     if mandante is None: return False;
 
